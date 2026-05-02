35c321042882a2d7653eb476822057902fcb1501
iconic101 <bibeshsedhain0@gmail.com>
2026-05-02 13:03:07 -0400

    prod fixes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4i0qcxfh#$5c2$4$(78hyg!+f317vg7m%*1*26l^nxbt*dpez+'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4i0qcxfh#$5c2$4$(78hyg!+f317vg7m%*1*26l^nxbt*dpez+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
